ffef73ff494999c8e809149fce39aba451dfd5e6
Tobzid <tobzid2013@gmail.com>
2022-06-30 17:18:58 -0700

    Test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#iclude <studio.h>
-
+#include <stdio.h>
 
 /**
- * main - print the size of various types
+ * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwize
  */
 int main(void)
 {
-put ("\"Programing is like building a multilingualpuzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
