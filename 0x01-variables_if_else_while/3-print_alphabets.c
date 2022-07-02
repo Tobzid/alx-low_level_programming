3662df2fd223b2ec28fb5ad7201446e870463372
Tobzid <tobzid2013@gmail.com>
2022-07-02 14:56:59 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,12 @@ int main(void)
 char letter;
 
 for (letter = 'a'; letter , = 'Z'; letter++)
-    putchar (letter);
+putchar (letter);
+
 for (letter ='A'; letter <= 'Z'; letter++)
-    putchar(letter);
+putchar(letter);
 
-   putchar('\n');
+putchar('\n');
 
-   return (0);
+return (0);
 }
