e68dd474f9ad6a3b8349a1400a3937ada89201fb
Tobzid <tobzid2013@gmail.com>
2022-07-03 10:33:55 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (ch = 'a'; ch <= 'f'; n++)
+{
+
+	putchar(n);
+}
+	for (ch = 'a'; ch <= 'f'; ch++)
+{
+
+	putchar(ch);
+}
+
+	putchar(10);
+
+	return(0);
+}
