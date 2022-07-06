783310c48d2e28a1ab5cbae316e8e712c2aabbc8
Tobzid <tobzid2013@gmail.com>
2022-07-06 16:47:04 -0700

    int _abs(int);

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs -> return absolute of a mumber
+ * @n : parameter
+ * Return: absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
