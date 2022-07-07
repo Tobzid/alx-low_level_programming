98c69db8bdc71623bc076840da735dbc4a42ba3b
Tobzid <tobzid2013@gmail.com>
2022-07-07 15:54:19 -0700

    Write a function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,7 +12,42 @@
  * jack_bauer - prints all minutes of the day
  * times_table - prints times table up to 9
  * print_to_98 - prints all natural numbers to 98
+ * print_times_table - print times table up to
+ * _isupper - checks if char is capital letter
+ * _isdigit - checks if int is a digit 0-9
+ * mul - multiply two numbers
+ * print_void - print numbers 0-9
+ * print_most_numbers - print 0-9 w/out 2 and 4
+ * more_numbers - print 0-14 ten times
+ * print int using only putchar
+ * print a line of n length
+ * print a diagonal line of n length
+ * print a square of n size
+ * print a triangle of n size
  */
 
 int _isupper(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_number(int);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
 
