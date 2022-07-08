1dae9941de21b35777bf5e323f628be8a4717830
Tobzid <tobzid2013@gmail.com>
2022-07-08 06:04:39 -0700

    a function that draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -46,8 +46,8 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_number(int);
-void print_line(int);
-void print_diagonal(int);
-void print_square(int);
-void print_triangle(int);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
