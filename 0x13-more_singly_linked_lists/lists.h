3381c90a111df8e2d10340977c3eb76a3461d42a
Tobzid <tobzid2013@gmail.com>
2022-08-19 15:57:36 -0700

    Print listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for Holberton project
- **/
+ */
 typedef struct listint_s
 {
-
 	int n;
 	struct listint_s *next;
 } listint_t;
@@ -22,6 +22,7 @@ typedef struct listint_s
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
+ *
  * Description: singly linked list of pointers
  */
 typedef struct listp_s
